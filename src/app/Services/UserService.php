b77f17d9e752b010328ed39b55f13ae9f800fea5
anarzone <haqverdiyevanar@gmail.com>
2022-01-26 22:20:54 +0400

    Update profile edit with daily reminder data

--- src/app/Services/UserService.php
+++ src/app/Services/UserService.php
@@ -3,15 +3,26 @@
 namespace App\Services;
 
 use App\Http\Resources\UserResource;
+use App\Models\Reminder;
 use App\Models\User;
 
 class UserService
 {
-    public function __construct(private User $userModel){}
+    public function __construct(private User $userModel, private Reminder $reminderModel){}
 
-    public function getDetails(): UserResource
+    public function getDetails(): array
     {
-        return new UserResource(auth()->user());
+        return [
+            'profile' => new UserResource(auth()->user()),
+            'reminders_daily' => [
+                'active' => $this->reminderModel->where('status',$this->reminderModel::STATUS_ACTIVE)
+                    ->whereRaw('DATE(created_at) = CURDATE()')->count(),
+                'done' => $this->reminderModel->where('status',$this->reminderModel::STATUS_DONE)
+                    ->whereRaw('DATE(created_at) = CURDATE()')->count(),
+                'missed' => $this->reminderModel->where('status',$this->reminderModel::STATUS_MISSED)
+                    ->whereRaw('DATE(created_at) = CURDATE()')->count(),
+            ]
+        ];
     }
 
     public function store($data): UserResource
